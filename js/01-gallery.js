82af9cd716b89361722d93907e8bd035a2bd8c5f
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-04-28 00:17:32 +0300

    markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+//=====отримуємо посилання на на елементи=========
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+//=====створюємо розмітку елементів галереї=======
+
+const createGallery = (arr) => {
+  const markup = [];
+  console.log(...galleryItems);
+  const item = [...arr]
+    .map(
+      (el) => `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt=${el.description}
+    />
+  </a>
+</div>`
+    )
+    .join("");
+  markup.push(item);
+  //   console.log(item);
+  refs.gallery.insertAdjacentHTML("afterbegin", markup);
+};
+console.log(createGallery(galleryItems));
+// console.log(galleryItems.prewiev);
